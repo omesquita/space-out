ca482aa19ecc698d6409c03cada83198703e0e33
Osnir Mesquita <contato@osnirmesquita.com.br>
2020-08-19 16:53:48 -0300

    Initial commit

---
+++ app/src/test/java/com/raywenderlich/android/spacingout/SpacingAnalyticsTest.kt
@@ -0,0 +1,26 @@
+package com.raywenderlich.android.spacingout
+
+import io.mockk.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SpacingAnalyticsTest {
+    @Test
+    fun `New analytic arguments are added before events are sent along`() {
+        mockkObject(ThirdPartyAnalyticsProvider)
+
+        val slot = slot<Map<String, String>>()
+
+        every { ThirdPartyAnalyticsProvider.logEvent(any(), capture(slot)) } just runs
+
+        SpacingAnalytics().logEvent("Test", mapOf("attribute" to "value"))
+
+        val expected = mapOf(
+            "attribute" to "value",
+            "client_type" to "Android",
+            "version" to "1"
+        )
+
+        assertEquals(expected, slot.captured)
+    }
+}
---
+++ app/src/test/java/com/raywenderlich/android/spacingout/images/ImagesViewModelTest.kt
@@ -0,0 +1,34 @@
+package com.raywenderlich.android.spacingout.images
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.raywenderlich.android.spacingout.CoroutinesTestRule
+import com.raywenderlich.android.spacingout.SpacingAnalytics
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
+import org.junit.Rule
+import org.junit.Test
+
+class ImagesViewModelTest {
+
+    @get:Rule
+    val coroutinesTestRule = CoroutinesTestRule()
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun `An analytics call is made when  images are loaded`() {
+        val provider = mockk<ImageListProvider>()
+        val analytics = spyk<SpacingAnalytics>()
+
+        every { analytics.logEvent(any(), any()) } answers {
+            println("You've changed the underlying log event method!")
+        }
+
+        val viewModel = ImagesViewModel(provider, analytics)
+
+        verify(exactly = 1) { analytics.logEvent("Fetching images") }
+    }
+}
